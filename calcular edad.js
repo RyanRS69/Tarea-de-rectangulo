7cac772666a43508f1f8035727f1d1ec0144ca38
RyanRS69 <114427319+RyanRS69@users.noreply.github.com>
2023-01-20 21:28:26 -0600

    Calcular edad con fecha de nacimiento

---
+++ calcular edad.js
@@ -0,0 +1,14 @@
+let person = {
+    name: null,
+
+    ape: null,
+
+    nacim: null,
+
+    }
+// code by ryan rocha salgado
+    edad = function(){
+        return 2023-person.nacim;
+    
+    }
+
