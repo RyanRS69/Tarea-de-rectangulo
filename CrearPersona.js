21b51d361ce5cff7e02b66b8854ebeca26088de8
RyanRS69 <114427319+RyanRS69@users.noreply.github.com>
2023-01-22 12:15:09 -0600

    Objeto de persona
    tarea objeto de pesona

---
+++ CrearPersona.js
@@ -0,0 +1,19 @@
+let persona = {
+    nombre: null,
+
+    apel: null,
+
+    curp: null,
+
+    edad: null,
+ }
+
+ presentacion = function (){
+ console.log(" Hola, soy "+persona.nombre+" , mi edad es "+ persona.edad);
+ }
+ 
+ datos = function (){
+    console.log("Nombre: "+persona.nombre+" apellidos: " +persona.apel+ " curp: "+ persona.curp + " ")
+ }
+
+ //Ryan Rocha Salgado 5223IS
